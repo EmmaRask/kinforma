1b807fd3c866bce021d3894e10cc89b2763304b5
emmafredling <emma.fredling@gmail.com>
2025-11-12 13:39:18 +0100

    update landingspage.php and add CSS filesS

--- landingapge.php
+++ landingapge.php
@@ -3,12 +3,139 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Kinforma test</title>
+    <link rel="stylesheet" href="CSS/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet" />
 </head>
+
 <body>
-    
+    <header>
+        <button class="menu-btn" aria-controls="site-menu" aria-expanded="false">
+            <img src="icons/Menu.svg" alt="" />
+        </button>
+        <nav class="header-nav">
+            <button class="language-opt">
+                <img src="icons/SV_EN.svg" alt="" />
+            </button>
+            <button class="shopping-cart">
+                <img src="icons/[00].svg" alt="" />
+            </button>
+        </nav>
+    </header>
+
+    <!-- <nav class="site-menu" id="site-menu">
+      <ul>
+        <li><a href="#">HEM</a></li>
+        <li><a href="#">KOLLEKTION</a></li>
+        <li><a href="#">NYHETSBREV</a></li>
+        <li><a href="#">OM OSS</a></li>
+        <li><a href="#">KONTAKT</a></li>
+      </ul>
+    </nav> -->
+    <main>
+        <section class="intro">
+            <img
+                class="logo"
+                src="icons/logo.svg"
+                alt="Kinformas logotype in color black with capital letters" />
+            <p class="scroll">[Scrolla för att utforska]</p>
+        </section>
+        <section class="hero"></section>
+        <section class="about">
+            <p class="tag-line">FRAMTIDENS MODE, IDAG</p>
+            <p class="headline-group">
+                <span class="right">Upplev en ny dimension av </span>
+                <span class="left">mode med vår senaste kollektion. </span>
+            </p>
+            <p class="sub-heading">
+                Designad av dig, drivet av teknik, <br />
+                skapat för planeten.
+            </p>
+            <p class="collection-name">KINFORMA KOLLEKTION 01</p>
+            <p class="description">
+                Kinforma föddes ur en enkel men kraftfull idé: att mode och teknik
+                tillsammans kan skapa en bättre framtid, för både människor och
+                planeten. Vårt namn kommer från två ord: “kin” som betyder släkt,
+                gemenskap och relationer, och “forma” som står för skapande,
+                förändring och design. Tillsammans bildar de vår grund: att forma
+                framtiden, tillsammans.
+            </p>
+            <button class="about-link">Lär mer om vår resa</button>
+        </section>
+
+        <section class="collection-products">
+            <div class="collection-header">
+                <p>KOLLEKTION 01</p>
+                <button>SE MER</button>
+            </div>
+            <!-- produkt 1 -->
+            <ul class="product-grid">
+                <li class="product">
+                    <article class="product">
+                        <img src="images/product-img.svg" alt="" />
+                        <p class="product-name">NAMN PÅ PRODUKT</p>
+                        <p class="product-price">SEK 10 000</p>
+                    </article>
+                </li>
+
+                <!-- produkt 2 -->
+                <li class="product">
+                    <article class="product">
+                        <img src="images/product-img.svg" alt="" />
+                        <p class="product-name">NAMN PÅ PRODUKT</p>
+                        <p class="product-price">SEK 10 000</p>
+                    </article>
+                </li>
+
+                <!-- produkt 3 -->
+                <li class="product">
+                    <article>
+                        <img src="images/product-img.svg" alt="" />
+                        <p class="product-name">NAMN PÅ PRODUKT</p>
+                        <p class="product-price">SEK 10 000</p>
+                    </article>
+                </li>
+
+                <!-- produkt 4 -->
+                <li class="product">
+                    <article>
+                        <img src="images/product-img.svg" alt="" />
+                        <p class="product-name">NAMN PÅ PRODUKT</p>
+                        <p class="product-price">SEK 10 000</p>
+                    </article>
+                </li>
+
+                <!-- produkt 5 -->
+                <li class="product">
+                    <article>
+                        <img src="images/product-img.svg" alt="" />
+                        <p class="product-name">NAMN PÅ PRODUKT</p>
+                        <p class="product-price">SEK 10 000</p>
+                    </article>
+                </li>
+
+                <!-- produkt 6 -->
+                <li class="product">
+                    <article>
+                        <img src="images/product-img.svg" alt="" />
+                        <p class="product-name">NAMN PÅ PRODUKT</p>
+                        <p class="product-price">SEK 10 000</p>
+                    </article>
+                </li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <h2>FOOTER</h2>
+    </footer>
 </body>
+
 </html>
