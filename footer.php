017ebe55c4211eb45f7e52957e9f1797d9f1be36
emmafredling <emma.fredling@gmail.com>
2025-11-26 21:18:26 +0100

    add my chances before pull

--- footer.php
+++ footer.php
@@ -37,7 +37,7 @@
     <a href="index.php" class="logo-footer"><img src="logo/kinforma-logo.svg" alt="Company logotype Kinforma"></a>
 </footer>
 
-<script src="javaScript/script.js"></script>
+<script src="javascript/script.js"></script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -2,67 +2,16 @@
 require __DIR__ . '/header.php';
 ?>
 
-<!-- MAIN -->
 <main>
     <!-- PRODUCTS PAIR -->
     <section class="collection-media">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Kinforma</title>
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet" />
-</head>
-
- <body>
-    <!-- HEADER -->
-    <header>
-        <div class="header-inner">
-            <!-- LOGOTYPE  -->
-            <a href="index.php" class="logo">
-                <img src="logo/kinforma-logo.svg" alt="Company logotype Kinforma">
-            </a>
-
-            <!-- NAVIGATION -->
-            <nav class="main-nav">
-                <ul class="fade-in-menu">
-                    <li class="item"><a href="">the gear</a></li>
-                    <li class="item"><a href="">the hub</a></li>
-                    <li class="item"><a href="">om oss</a></li>
-                    <li class="item"><a href="">kontakt</a></li>
-                    <li class="item"><a href="">SV | <span class="inactive-lang">EN</span></a></li>
-                </ul>
-            </nav>
-
-            <!-- ICONS -->
-            <menu class="header-icons">
-                <li> <a href="#" class="cart">[00]</a></li>
-
-                <button class="hamburger">
-                    <img class="menu-icon" src="icons/menu-icon.svg" alt="Meny ikon">
-                    <img class="close-icon" src="icons/close-icon.svg" alt="Stänga menyn">
-                </button>
-            </menu>
-        </div>
-
-    </header>
-
-    <!-- MAIN -->
-    <main>
-        <!-- PRODUCTS PAIR -->
-        <section class="collection-media">
-
-            <section class="pair">
-                <img class="product-image js-product-image"
-                    src="images/backpack/backpack-beige.jpg" alt=""
-                    data-folder="backpack"
-                    data-name="backpack"
-                    data-color="beige" />
+        <section class="pair">
+            <img class="product-image js-product-image"
+                src="images/backpack/backpack-beige.jpg" alt=""
+                data-folder="backpack"
+                data-name="backpack"
+                data-color="beige" />
 
 
             <div class="color-picker">
