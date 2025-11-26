eb7bae9b1490b8323a67587c7b9a9f5330a0981f
emmafredling <emma.fredling@gmail.com>
2025-11-26 13:28:37 +0100

    sorting files add header.php + footer.php

---
+++ footer.php
@@ -0,0 +1,43 @@
+<!-- FOOTER -->
+<footer>
+
+    <!-- NEWSLETTER -->
+    <section class="newsletter">
+        <h3>nyhetsbrev</h3>
+        <p>Anmäl dig till vårt nyhetsbrev, få tillgång till nya släpp och få 10% rabatt på din första beställning.</p>
+        <form>
+            <input type="email" id="newsletter-email" name="email" autocomplete="email" placeholder="Skriv in din e-postadress">
+            <button type="reset" class="newsletter-btn"><img src="icons/arrow-forward.svg" alt=""></button>
+            <br>
+
+        </form>
+
+    </section>
+
+    <nav>
+        <h3>kundtjänst</h3>
+        <ul>
+            <li><a href="javascript:void(0)">kontakta oss</a></li>
+            <li><a href="javascript:void(0)">allmänna villkor</a></li>
+            <li><a href="javascript:void(0)">leverans & returer</a></li>
+            <li><a href="javascript:void(0)">integritetspolicy</a></li>
+        </ul>
+    </nav>
+
+    <nav>
+        <h3>om oss</h3>
+        <ul>
+            <li><a href="javascript:void(0)">butiker</a></li>
+            <li><a href="javascript:void(0)">the hub</a></li>
+            <li><a href="javascript:void(0)">the gear</a></li>
+            <li><a href="javascript:void(0)">hållbarhet</a></li>
+        </ul>
+    </nav>
+
+    <a href="index.php" class="logo-footer"><img src="logo/kinforma-logo.svg" alt="Company logotype Kinforma"></a>
+</footer>
+
+<script src="JavaScript/script.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Kinforma</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet" />
+</head>
+
+<body>
+
+    <!-- HEADER -->
+    <header>
+        <div class="header-inner">
+            <!-- LOGOTYPE  -->
+            <a href="index.php" class="logo">
+                <img src="logo/kinforma-logo.svg" alt="Company logotype Kinforma">
+            </a>
+
+            <!-- NAVIGATION -->
+            <nav class="main-nav">
+                <ul class="fade-in-menu">
+                    <li class="item"><a href="">the gear</a></li>
+                    <li class="item"><a href="">the hub</a></li>
+                    <li class="item"><a href="">om oss</a></li>
+                    <li class="item"><a href="">kontakt</a></li>
+                    <li class="item"><a href="">SV | <span class="inactive-lang">EN</span></a></li>
+                </ul>
+            </nav>
+
+            <!-- ICONS -->
+            <menu class="header-icons">
+                <li> <a href="#" class="cart">[00]</a></li>
+
+                <button class="hamburger">
+                    <img class="menu-icon" src="icons/menu-icon.svg" alt="Meny ikon">
+                    <img class="close-icon" src="icons/close-icon.svg" alt="Stänga menyn">
+                </button>
+            </menu>
+        </div>
+
+    </header>
