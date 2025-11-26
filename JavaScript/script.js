49b93990b7f17a09ecf80e1c7db8e2e2387111a8
emmafredling <emma.fredling@gmail.com>
2025-11-26 13:45:21 +0100

    chance javascript + css

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -60,7 +60,12 @@ products.forEach((productImage) => {
   });
 });
 
+const isDesktop = window.matchMedia("(min-width: 900px)").matches;
+
 document.querySelectorAll(".pair").forEach((pair) => {
+  // hoppa över allt i desktop-läge
+  if (isDesktop) return;
+
   const video = pair.querySelector(".media-video");
   const img = pair.querySelector(".product-image");
   const playBtn = pair.querySelector(".play-btn");
@@ -90,3 +95,34 @@ document.querySelectorAll(".pair").forEach((pair) => {
     playBtn.style.display = "block";
   }
 });
+
+/*/document.querySelectorAll(".pair").forEach((pair) => {
+  const video = pair.querySelector(".media-video");
+  const img = pair.querySelector(".product-image");
+  const playBtn = pair.querySelector(".play-btn");
+
+  // Skip if something is missing
+  if (!video || !img || !playBtn) return;
+
+  // Start video on click
+  playBtn.addEventListener("click", () => {
+    img.style.display = "none";
+    playBtn.style.display = "none";
+    video.style.display = "block";
+    video.play();
+  });
+
+  // Reset on click on video
+  video.addEventListener("click", reset);
+
+  // Reset when video ends
+  video.addEventListener("ended", reset);
+
+  function reset() {
+    video.pause();
+    video.currentTime = 0;
+    video.style.display = "none";
+    img.style.display = "block";
+    playBtn.style.display = "block";
+  }
+});/*/
